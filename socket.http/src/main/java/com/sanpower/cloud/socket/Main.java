3c97cdf160a7724c4910971101139ce960dae8fb
Administrator <Administrator@fnst-pc>
2014-06-27 15:08:33 +0800

    修改Main

--- socket.http/src/main/java/com/sanpower/cloud/socket/Main.java
+++ socket.http/src/main/java/com/sanpower/cloud/socket/Main.java
@@ -5,8 +5,5 @@ import com.sanpower.cloud.socket.business.Business;
 public class Main {
 	public static void main(String[] args) {
 		new Business().exec();
-		/*String url = "http://172.10.2.190:8081/search/456.json";
-		String collectionName = url.substring(url.lastIndexOf("/")+1,url.lastIndexOf("."));
-		System.out.println(collectionName);*/
 	}
 }
